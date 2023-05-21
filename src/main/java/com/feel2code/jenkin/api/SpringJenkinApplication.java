73a37948ddb61268821cd799e891806b6f06ecb0
Hariom Mukati <c_vikash.jaiswal@infinitylearn.com>
2023-05-21 11:53:49 +0530

    first Commit

---
+++ src/main/java/com/feel2code/jenkin/api/SpringJenkinApplication.java
@@ -0,0 +1,26 @@
+package com.feel2code.jenkin.api;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class SpringJenkinApplication {
+	
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinApplication.class);
+	
+	@PostConstruct
+	public void init() {
+		logger.info("Application started.");
+	}
+
+	public static void main(String[] args) {
+		
+		logger.info("Application Executed.");
+		SpringApplication.run(SpringJenkinApplication.class, args);
+	}
+
+}
